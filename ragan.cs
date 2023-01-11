bb2eff7cbfde26da17e553973e23d5fb871497e3
khalid-khan-panezai <khalidkhankakar777@gmail.com>
2023-01-12 01:56:39 +0500

    new

---
+++ ragan.cs
@@ -0,0 +1,39 @@
+img {
+    width: 50px;
+  }
+  
+  header {
+    width: 80%;
+    margin: 0 auto;
+  }
+  
+  nav a {
+    margin-left: 20px;
+  }
+  
+  iframe {
+    display: block;
+  
+    margin: 0 auto;
+    margin-top: 40px;
+  }
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  form {
+    margin-top: 40px;
+  }
+  input {
+    display: block;
+    margin-bottom: 20px;
+    padding: 10px;
+  }
+  
+  @media only screen and (max-width: 600px) {
+    iframe {
+      width: 200;
+      height: 200;
+    }
+  }
